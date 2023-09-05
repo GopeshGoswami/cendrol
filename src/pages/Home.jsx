e8a0cff82b27dab23a6748c858dc052b44f01a5e
Gopesh Goswami <ggopesh45@gmail.com>
2023-09-06 04:21:23 +0530

    Loaded added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import Cards from "../components/Cards";
 import axios from "axios";
 import Card from "../components/Card";
@@ -9,12 +10,20 @@ const Home = () => {
   const [joke, setJoke] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [jokeLoading, setJokeLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("https://api.chucknorris.io/jokes/categories")
-      .then((response) => setCategories(response.data))
-      .catch((response) => console.log("Error fetching Cards", response));
+      .then((response) => {
+        setCategories(response.data);
+        setIsLoading(false);
+      })
+      .catch((response) => {
+        console.log("Error fetching Cards", response);
+        setIsLoading(false);
+      });
   }, []);
 
   const handleCardClick = (category) => {
@@ -35,6 +44,7 @@ const Home = () => {
       .get(`https://api.chucknorris.io/jokes/random?category=${category}`)
       .then((response) => {
         setJoke(response.data.value);
+        setJokeLoading(false);
         console.log(joke);
       })
       .catch((error) => console.error("Error fetching joke:", error));
@@ -48,10 +58,22 @@ const Home = () => {
 
   return (
     <>
+      {isLoading ? (
+        <AiOutlineLoading3Quarters className="animate-spin text-white text-9xl" />
+      ) : (
+        ""
+      )}
+
       <div className="grid  grid-cols-4 md:grid-cols-2 lg:grid-cols-4 text-white text-lg md:gap-y-3  md:w-fit  bg-transparent">
         {categories.map((category, index) => {
           return (
-            <div key={index} onClick={() => handleCardClick(category)}>
+            <div
+              key={index}
+              onClick={() => {
+                handleCardClick(category);
+                setJokeLoading(true);
+              }}
+            >
               <Cards
                 category={category}
                 isOpen={isModalOpen}
@@ -67,6 +89,8 @@ const Home = () => {
         onClose={closeModal}
         joke={joke}
         fetchJokeForCategory={fetchJokeForCategory}
+        jokeLoading={jokeLoading}
+        setJokeLoading={setJokeLoading}
       />
     </>
   );
