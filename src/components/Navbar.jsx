724bf986367cc2ad04f3d5a8f496ddae992d5a5e
Gopesh Goswami <ggopesh45@gmail.com>
2023-09-06 03:35:34 +0530

    Navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <h1 className="text-4xl text-green-500 font-bold m3 animate-bounce">
+      <h1 className="text-4xl text-green-500 font-bold m-3 animate-bounce">
         Chuck Norries
       </h1>
     </div>
